8ae17c0771db8f1cb8951676ff67708e0d8dcc70
Manan Aggarwal <aggarwalmanan4@gmail.com>
2025-12-23 10:02:29 +0530

    Removed Unused Fonts

--- app/layout.js
+++ app/layout.js
@@ -12,15 +12,7 @@ const poppins = Poppins({
     subsets: ["latin"],
     weight: ['300', '400', '500', '600', '700', '800'],
 });
-const lexend = Lexend({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-lexend', // Define a CSS variable
-})
-const inter = Inter({
-    subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-})
+
 export const metadata = {
     title: "Catalyst Clinical Services",
     description: "Web App created by Myarth Tech ( https://myarth.in )",
@@ -29,7 +21,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body className={`body ${poppins.className} ${lexend.variable} ${inter.variable}`}>
+            <body className={`body ${poppins.className}`}>
                 <ContextWrapper>{children}</ContextWrapper>
                 {/* <ToastContainer /> */}
             </body>
