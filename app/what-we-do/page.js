ac95002f8fb4382fadbd7c2b4f87b084fcc1aafa
Sakshamebdev873 <vinodarya344@gmail.com>
2025-12-30 16:23:20 +0530

    fixed the static color by global css and little changes..

--- app/what-we-do/page.js
+++ app/what-we-do/page.js
@@ -53,8 +53,6 @@ export default function WhatWeDo() {
                   </Link>
                 </div>
                 </div>
-
-                {/* Content */}
                 
                 </div>
               
