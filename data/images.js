32cbf71162045570697b307d5ad6cc5176243d72
Sakshamebdev873 <vinodarya344@gmail.com>
2026-01-14 18:42:23 +0530

    started working on new page of service detail will complete soon

--- data/images.js
+++ data/images.js
@@ -45,13 +45,14 @@ const placementLogos = {baxter, accenture, lilly, bayer, fortis, abbott} ;
 /* Being used in CSS, not directly */
 
 import portfolioBgLight from '@p/portfolio-bg-light.png' ;
-
+import radient from '@p/radient-gradient.png'
 // services
 import DentalClinic from '@p/services/DentalClinic.png'
 import Cardiomyopathy from '@p/services/Cardiomyopathy.png'
 import Hematology from '@p/services/Hematology.png'
 import Neurology from '@p/services/Neurology.png'
 import Orthopedics from '@p/services/Orthopedics.png'
+import serviceImg from '@p/newServiceImage.png'
 import Pharmacology from '@p/services/Pharmacology.png'
 // services icon
 import d1 from '@p/services/d1.png'
@@ -71,4 +72,4 @@ import Round2 from '@p/About/round2.png'
 import Round3 from '@p/About/round3.png'
 import headIcon from '@p/About/headIcon.png'
 export {
-  def, myarth,DentalClinic, headIcon, Cardiomyopathy ,Round , Round1 ,Round2 ,Round3 ,Ortho ,Doctor ,d1 ,c1 ,h1 ,n1 ,o1 ,p1 ,Nurses ,Hematology ,Neurology ,Orthopedics ,Pharmacology ,blogBannerImage, blog2, contact, titleLine, portfolioBgLight, logo, homeBanner, about1, about2, exp1, exp2, exp3, pub1, pub2, pub3, expertiseBg, cardImage, pragramBanner, programs, about3, awards, dummyLogo, event1, event2, event3, course, placementLogos};
+  def, myarth,DentalClinic, headIcon,radient, serviceImg,Cardiomyopathy ,Round , Round1 ,Round2 ,Round3 ,Ortho ,Doctor ,d1 ,c1 ,h1 ,n1 ,o1 ,p1 ,Nurses ,Hematology ,Neurology ,Orthopedics ,Pharmacology ,blogBannerImage, blog2, contact, titleLine, portfolioBgLight, logo, homeBanner, about1, about2, exp1, exp2, exp3, pub1, pub2, pub3, expertiseBg, cardImage, pragramBanner, programs, about3, awards, dummyLogo, event1, event2, event3, course, placementLogos};
