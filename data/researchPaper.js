c6d4152db791eab142ce4df44739bac1e0fa7ca8
Sumit-Singh94 <sumitsin3122@gmail.com>
2026-01-14 22:33:37 +0530

    changes made.

--- data/researchPaper.js
+++ data/researchPaper.js
@@ -1,3 +1,4 @@
+import {images,pdfs} from "./researchpaperimagespdf";
 import { about1 } from "@images";
 import {
   DentalClinic,
@@ -8,54 +9,167 @@ import {
   Pharmacology,
 } from "@images";
 
+
 export const researchPaperData = [
   {
     id: "ai-medical-diagnosis",
     heading: "AI-Based Early Diagnosis of Cardiovascular Diseases",
     description:
       "This paper explores machine learning models for early detection of cardiovascular diseases using patient vitals, ECG signals, and clinical data.",
-    image: Cardiomyopathy,
-    link: "https://example.com/ai-medical-diagnosis.pdf",
+    image: images.img1,
+    link: pdfs.pdf1,
   },
   {
     id: "neuro-rehab",
     heading: "Advancements in Neurological Rehabilitation Techniques",
     description:
       "A systematic review of modern neuro-rehabilitation methods focusing on stroke recovery, neural plasticity, and assistive technologies.",
-    image: Neurology,
-    link: "https://example.com/ai-medical-diagnosis.pdf",
+    image: images.img2,
+    link: pdfs.pdf2,
   },
   {
     id: "ortho-implants",
     heading: "Biomechanical Analysis of Modern Orthopedic Implants",
     description:
       "An analytical study on the durability, material science, and biomechanical performance of next-generation orthopedic implants.",
-    image: Orthopedics,
-    link: "https://example.com/ai-medical-diagnosis.pdf",
+    image: images.img3,
+    link: pdfs.pdf3,
   },
   {
     id: "hematology-therapy",
     heading: "Targeted Therapy Approaches in Hematological Disorders",
     description:
       "This research evaluates personalized treatment strategies in hematological diseases using genetic and molecular profiling.",
-    image: Hematology,
-    link: "https://example.com/ai-medical-diagnosis.pdf",
+    image: images.img4,
+    link: pdfs.pdf4,
   },
   {
     id: "drug-delivery",
     heading: "Nanotechnology in Controlled Drug Delivery Systems",
     description:
       "An in-depth exploration of nanoparticle-based drug delivery mechanisms and their impact on treatment efficiency.",
-    image: Pharmacology,
-    link: "https://example.com/ai-medical-diagnosis.pdf",
+    image: images.img5,
+    link: pdfs.pdf5,
   },
   {
     id: "oral-health-study",
     heading: "Long-Term Impact of Preventive Dental Care on Oral Health",
     description:
       "A longitudinal study assessing preventive dental care practices and their role in reducing chronic oral diseases.",
-    image: DentalClinic,
-    link: "https://example.com/ai-medical-diagnosis.pdf",
+    image: images.img6,
+    link: pdfs.pdf6,
+  },
+  {
+    id: "oral-health-study",
+    heading: "Long-Term Impact of Preventive Dental Care on Oral Health",
+    description:
+      "A longitudinal study assessing preventive dental care practices and their role in reducing chronic oral diseases.",
+    image: images.img7,
+    link: pdfs.pdf7,
+  },
+  {
+    id: "oral-health-study",
+    heading: "Long-Term Impact of Preventive Dental Care on Oral Health",
+    description:
+      "A longitudinal study assessing preventive dental care practices and their role in reducing chronic oral diseases.",
+    image: images.img8,
+    link: pdfs.pdf8,
+  },
+  {
+    id: "oral-health-study",
+    heading: "Long-Term Impact of Preventive Dental Care on Oral Health",
+    description:
+      "A longitudinal study assessing preventive dental care practices and their role in reducing chronic oral diseases.",
+    image: images.img9,
+    link: pdfs.pdf9,
+  },
+  {
+    id: "oral-health-study",
+    heading: "Long-Term Impact of Preventive Dental Care on Oral Health",
+    description:
+      "A longitudinal study assessing preventive dental care practices and their role in reducing chronic oral diseases.",
+    image: images.img10,
+    link: pdfs.pdf10,
+  },
+  {
+    id: "oral-health-study",
+    heading: "Long-Term Impact of Preventive Dental Care on Oral Health",
+    description:
+      "A longitudinal study assessing preventive dental care practices and their role in reducing chronic oral diseases.",
+    image: images.img11,
+    link: pdfs.pdf11,
+  },
+  {
+    id: "oral-health-study",
+    heading: "Long-Term Impact of Preventive Dental Care on Oral Health",
+    description:
+      "A longitudinal study assessing preventive dental care practices and their role in reducing chronic oral diseases.",
+    image: images.img12,
+    link: pdfs.pdf12,
+  },
+  {
+    id: "oral-health-study",
+    heading: "Long-Term Impact of Preventive Dental Care on Oral Health",
+    description:
+      "A longitudinal study assessing preventive dental care practices and their role in reducing chronic oral diseases.",
+    image: images.img13,
+    link: pdfs.pdf13,
+  },
+  {
+    id: "oral-health-study",
+    heading: "Long-Term Impact of Preventive Dental Care on Oral Health",
+    description:
+      "A longitudinal study assessing preventive dental care practices and their role in reducing chronic oral diseases.",
+    image: images.img14,
+    link: pdfs.pdf14,
+  },
+  {
+    id: "oral-health-study",
+    heading: "Long-Term Impact of Preventive Dental Care on Oral Health",
+    description:
+      "A longitudinal study assessing preventive dental care practices and their role in reducing chronic oral diseases.",
+    image: images.img15,
+    link: pdfs.pdf15,
+  },
+  {
+    id: "oral-health-study",
+    heading: "Long-Term Impact of Preventive Dental Care on Oral Health",
+    description:
+      "A longitudinal study assessing preventive dental care practices and their role in reducing chronic oral diseases.",
+    image: images.img16,
+    link: pdfs.pdf16,
+  },
+  {
+    id: "oral-health-study",
+    heading: "Long-Term Impact of Preventive Dental Care on Oral Health",
+    description:
+      "A longitudinal study assessing preventive dental care practices and their role in reducing chronic oral diseases.",
+    image: images.img17,
+    link: pdfs.pdf17,
+  },
+  {
+    id: "oral-health-study",
+    heading: "Long-Term Impact of Preventive Dental Care on Oral Health",
+    description:
+      "A longitudinal study assessing preventive dental care practices and their role in reducing chronic oral diseases.",
+    image: images.img18,
+    link: pdfs.pdf18,
+  },
+  {
+    id: "oral-health-study",
+    heading: "Long-Term Impact of Preventive Dental Care on Oral Health",
+    description:
+      "A longitudinal study assessing preventive dental care practices and their role in reducing chronic oral diseases.",
+    image: images.img19,
+    link: pdfs.pdf19,
+  },
+  {
+    id: "oral-health-study",
+    heading: "Long-Term Impact of Preventive Dental Care on Oral Health",
+    description:
+      "A longitudinal study assessing preventive dental care practices and their role in reducing chronic oral diseases.",
+    image: images.img20,
+    link: pdfs.pdf20,
   },
 ];
 
---
+++ data/researchpaperimagespdf.js
@@ -0,0 +1,53 @@
+const images = {
+  img1:  '/researchimage/p-1.jpg',
+  img2:  '/researchimage/p-2.jpg', 
+  img3:  '/researchimage/p-3.jpg',
+  img4:  '/researchimage/p-4.jpg',
+  img5:  '/researchimage/p-5.jpg',
+  img6:  '/researchimage/p-6.jpg',
+  img7:  '/researchimage/p-7.png',
+  img8:  '/researchimage/p-8.png',
+  img9:  '/researchimage/p-9.png',
+  img10: '/researchimage/p-10.png',
+  img11: '/researchimage/p-11.jpg',
+  img12: '/researchimage/p-12.jpg',
+  img13: '/researchimage/p-13.jpg',
+  img14: '/researchimage/p-14.jpg',
+  img15: '/researchimage/p-15.jpg',
+  img16: '/researchimage/p-16.jpg',
+  img17: '/researchimage/p-17.jpg',
+  img18: '/researchimage/p-18.jpg',
+  img19: '/researchimage/p-19.jpg',
+  img20: '/researchimage/p-20.jpg',
+};
+
+
+
+const pdfs = {
+  pdf1:  '/researchpdf/p1.pdf', 
+  pdf2:  '/researchpdf/p2.pdf',
+  pdf3:  '/researchpdf/p3.pdf',
+  pdf4:  '/researchpdf/p4.pdf',
+  pdf5:  '/researchpdf/p5.pdf',
+  pdf6:  '/researchpdf/p6.pdf',
+  pdf7:  '/researchpdf/p7.pdf',
+  pdf8:  '/researchpdf/p8.pdf',
+  pdf9:  '/researchpdf/p9.pdf',
+  pdf10: '/researchpdf/p10.pdf',
+  pdf11: '/researchpdf/p11.pdf',
+  pdf12: '/researchpdf/p12.pdf',
+  pdf13: '/researchpdf/p13.pdf',
+  pdf14: '/researchpdf/p14.pdf',
+  pdf15: '/researchpdf/p15.pdf',
+  pdf16: '/researchpdf/p16.pdf',
+  pdf17: '/researchpdf/p17.pdf',
+  pdf18: '/researchpdf/p18.pdf',
+  pdf19: '/researchpdf/p19.pdf',
+  pdf20: '/researchpdf/p20.pdf',
+};
+
+
+
+
+
+export {images,pdfs}
